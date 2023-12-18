4b80496dce21197ee951bf4933a7d9f4bd37a79f
HorchataChufy <153616140+HorchataChufy@users.noreply.github.com>
2023-12-18 09:58:48 +0100

    Add files via upload

---
+++ Codigo/ sensores/Pluviometro/pluviometromain.cpp
@@ -0,0 +1,42 @@
+const int SENSOR=3;
+const int umbraltiempo=300;
+volatile int ISRContador = 0;
+int contador = 0;
+float litros=0;
+long tiempocontador=0;
+long tiempohora =0;
+
+void setup(){
+   pinMode(SENSOR, INPUT_PULLUP);
+   Serial.begin(9600);
+   attachInterrupt(digitalPinToInterrupt(SENSOR), contadorLitros, FALLING);
+   Serial.begin(115200);
+  Serial.println(F("DHT 11 prueba de conexión con el servidor"));
+}
+void contadorLitros()
+{
+ if(millis()>(tiempocontador+umbraltiempo))
+  {
+    ISRContador++;
+    tiempocontador=millis();
+    Serial.println(ISRContador);
+  }
+}
+void loop()  {
+ if (millis()<tiempohora)
+ {
+    tiempohora=0;
+    }
+ if (millis()-tiempohora>10000)
+ { 
+    tiempohora = millis();
+    litros=ISRContador *0.28;
+    if (litros>0)
+    {
+    Serial.print("Litros por m2 caidos en una una Hora (Lm2)" );
+    Serial.println(litros);
+    ISRContador= 0;
+    contador= 0;
+    }
+  }
+}
